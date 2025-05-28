852f971f5514db4aeb0403d8e7ee0f007328fdfa
ge <grigoryermizin@gmail.com>
2025-05-27 18:46:53 -0700

    magic number generator

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdio.h>
+#include <string.h>
 
 using std::cout;
 using U64 = unsigned long long;
@@ -37,6 +38,9 @@ using U64 = unsigned long long;
 
  //sides to move (colors)
  enum{white, black};
+
+ //bishop and rook
+ enum{rook, bishop};
 
  const char *square_to_coordinates[] ={
     "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
@@ -176,6 +180,141 @@ const int rook_relevant_bits[64] = {
     12, 11, 11, 11, 11, 11, 11, 12
 };
 
+// rook magic numbers
+U64 rook_magic_numbers[64] = {
+    0x2000380201000008ULL,
+    0x9002041009000030ULL,
+    0x1000430100100082ULL,
+    0x808008040002020ULL,
+    0x801a0105000424ULL,
+    0x12000018200000a0ULL,
+    0x2000020800220040ULL,
+    0x40801910d4004ULL,
+    0x1020210000040180ULL,
+    0x1915500080810013ULL,
+    0x2800000882600ULL,
+    0x28140010110aULL,
+    0xc0018c1000262001ULL,
+    0xa001000200060ULL,
+    0xc00000e00a000500ULL,
+    0x4050008210040800ULL,
+    0x25c080804200101ULL,
+    0x14a0000c00001004ULL,
+    0x202008011001182ULL,
+    0x88d288c004000ULL,
+    0xa520042000120000ULL,
+    0x80280304a109080ULL,
+    0x841801400120030ULL,
+    0x800080000100001ULL,
+    0x3200a40580200001ULL,
+    0x400020500c00200ULL,
+    0x2020020410000010ULL,
+    0x1000082400000400ULL,
+    0x800402808004000ULL,
+    0x40000300a102020ULL,
+    0x80042002904000ULL,
+    0x181080480000041ULL,
+    0x540004900470302ULL,
+    0x2019004000c382ULL,
+    0x1040010001880800ULL,
+    0x9000200289008000ULL,
+    0x131042080000150ULL,
+    0x220800040c04800ULL,
+    0x8c02100001200ULL,
+    0x2022188000040120ULL,
+    0x20020891201800ULL,
+    0x608022088440411ULL,
+    0x8061840001108005ULL,
+    0x870001a040ULL,
+    0x7000000009018ULL,
+    0x1001064a2250000ULL,
+    0x2000820300180020ULL,
+    0x2040c09000060020ULL,
+    0x2008000004069210ULL,
+    0x8b08001220009ULL,
+    0x400032040004ULL,
+    0x20006200040280c0ULL,
+    0x20004403000010ULL,
+    0x8000c001020c0001ULL,
+    0x12040810902240ULL,
+    0x400c802211420200ULL,
+    0x1a10004004004ULL,
+    0xa09540042500000ULL,
+    0x540008008069ULL,
+    0x1080c804200ULL,
+    0xa000081241180380ULL,
+    0x800880000402445ULL,
+    0x8040035200202040ULL,
+    0x400080002440020ULL
+};
+
+//bishop magic numbers
+U64 bishop_magic_numbers[64] = {
+    0x8000900140840092ULL,
+    0x804840004c900ULL,
+    0x4018a4000100200ULL,
+    0x2008028800020000ULL,
+    0x4003108c18304c34ULL,
+    0x2c08401405280000ULL,
+    0x100780600010ULL,
+    0xc408004a001040ULL,
+    0x800420008018010ULL,
+    0x1800012898000018ULL,
+    0x62040981c0001032ULL,
+    0x102120030402100ULL,
+    0x10182840048180ULL,
+    0x50405800000840ULL,
+    0x4300085008ULL,
+    0x11008402150a0200ULL,
+    0x840011004044280ULL,
+    0x704840810202ULL,
+    0x8000020e080001ULL,
+    0x8808008200808401ULL,
+    0x44c20212800000ULL,
+    0x18000d090041800ULL,
+    0x10e020504211200ULL,
+    0x200021018440400ULL,
+    0xaa0001210064048ULL,
+    0x241102000008124ULL,
+    0x100002c12180204ULL,
+    0x880200102070ULL,
+    0x40200052008011ULL,
+    0x430000d0ULL,
+    0x8024c0005408060fULL,
+    0x400002000c50060ULL,
+    0x422008180c1020ULL,
+    0x2308102a00414400ULL,
+    0x2000082020004206ULL,
+    0x8004000c2024090ULL,
+    0x6006501801ULL,
+    0x88880004685010ULL,
+    0x10000802040003c0ULL,
+    0x1010430800024000ULL,
+    0x410041240190800ULL,
+    0x1804000b00810100ULL,
+    0x800002808000284ULL,
+    0x648041000ULL,
+    0x80010802081044ULL,
+    0x8080024009001042ULL,
+    0x3000dc020500020ULL,
+    0x80042000e00004ULL,
+    0x220802468032200ULL,
+    0x10858020080a0ULL,
+    0x2080604201818000ULL,
+    0x8002100214000ULL,
+    0x8040000821802000ULL,
+    0xa0001200000c020ULL,
+    0x100090004e800ULL,
+    0x28890000282040ULL,
+    0x40502920100000ULL,
+    0x8020293001200840ULL,
+    0x2605400040003000ULL,
+    0x2200002002800446ULL,
+    0x200806200a008ULL,
+    0x801400600042100ULL,
+    0x2900290080208400ULL,
+    0x5000020058ULL
+};
 
 // pawn attacks table [side][square]
 U64 pawn_attacks[2][64];
@@ -185,7 +324,6 @@ U64 knight_attacks[64];
 
 //kings attacks table [square]
 U64 king_attacks[64];
-
 
 // generate pawn attacks
 U64 mask_pawn_attacks(int side, int square){
@@ -432,22 +570,20 @@ U64 set_occupancy(int index, int bits_in_mask, U64 attack_mask){
     return occupancy;
 }
 
-
-
 /*********************************\ 
  
-        Main Driver
+         Magics
 
  ==================================
  \*********************************/ 
 
  //psuedo random number state
-unsigned int state = 1804289383;
+unsigned int random_state = 1804289383;
 
 // gen 32-bit psudo legal numbers
 unsigned int get_random_U32number(){
     // get current state
-    unsigned int number = state;
+    unsigned int number = random_state;
 
     //XOR shift algorithm
     number ^= number << 13;
@@ -455,11 +591,11 @@ unsigned int get_random_U32number(){
     number ^= number << 5;
 
     //upadte rnadom number state
-    state = number;
+    random_state = number;
     
     //return random number
 
-    return state;
+    return number;
 }
 
 //generate 64 bit pseudo legal numbers function
@@ -469,10 +605,10 @@ U64 get_random_U64number(){
 
     U64 n1, n2, n3, n4;
 
-    n1 = (U64)get_random_U32number() & 0xFFFF;
-    n2 = (U64)get_random_U32number() & 0xFFFF;
-    n3 = (U64)get_random_U32number() & 0xFFFF;
-    n4 = (U64)get_random_U32number() & 0xFFFF;
+    n1 = (U64)(get_random_U32number()) & 0xFFFF;
+    n2 = (U64)(get_random_U32number()) & 0xFFFF;
+    n3 = (U64)(get_random_U32number()) & 0xFFFF;
+    n4 = (U64)(get_random_U32number()) & 0xFFFF;
 
     return n1 | (n2 << 16) | (n3 << 32) | (n4 << 48);
 }
@@ -482,15 +618,124 @@ U64 generate_magic_number(){
     return get_random_U64number() & get_random_U64number() & get_random_U64number();
 }
 
-int main(){
+ //find apporpriate magic number
 
-    //initialize leaper pieces attacks
+ U64 find_magic_number(int square, int relevant_bits, int bishop){
+    // init occupancy
+    U64 occupancies[4096];
+
+    //init attack tables
+    U64 attacks[4096];
+
+    //init used attacks
+    U64 used_attacks[4096];
+
+    //init attack mask for a current piece
+    U64 attack_mask = bishop ? mask_bishop_attacks(square) : mask_rook_attacks(square);
+
+    int occupancy_indicies = 1 << relevant_bits;
+
+    //loop over occupancy indicies
+    for(int index = 0; index < occupancy_indicies; index++){
+        //init occupancies
+        occupancies[index] = set_occupancy(index, relevant_bits, attack_mask);
+
+        //innit attacks 
+        attacks[index] = bishop ? bishop_attacks_on_the_fly(square, occupancies[index]) 
+                                : rook_attacks_on_the_fly(square, occupancies[index]);
+    }
+
+    //test magic numbers loop
+
+    for(int random_count = 0; random_count < 1000000000; random_count++){
+        //gen magic number candidate
+        U64 magic_number = generate_magic_number();
+
+        // skip inappropriate candidates
+        if(count_bits((attack_mask * magic_number) & 0xFF00000000000000) < 6) continue;
+
+        //init used attacks array
+        memset(used_attacks, 0ULL, sizeof(used_attacks));
+
+        //init inde & fail flag
+        int index, fail;
+
+        //test magic index loop
+        for(index = 0, fail = 0; !fail && index < occupancy_indicies; index++){
+            //init magic index
+            int magic_index = (int)((occupancies[index] * magic_number) >> (64 - relevant_bits));
+
+            //if magic index works
+            if(used_attacks[magic_index] == 0ULL){
+                //init used attacks
+                used_attacks[magic_index] = attacks[magic_index];
+            }
+
+            //otherwise
+            else if(used_attacks[magic_index] != attacks[magic_index]){
+                fail = 1;
+            }
+        }
+        //if magic number works return it
+        if(!fail)
+            return magic_number;
+    }
+    //if magic number doesnt work
+    printf(" Magic number fails");
+    return 0ULL;
+ }
+
+//init magic numbers
+
+
+
+void init_magic_numbers(){
+    //loop over 64 board squares
+
+    for(int square = 0; square < 64; square++){
+        //init rookmagic numbers
+        //printf(" 0x%llxULL\n", find_magic_number(square, rook_relevant_bits[square], rook));
+        rook_magic_numbers[square] = find_magic_number(square, rook_relevant_bits[square], rook);
+    }
+
+    std::cout <<  "\n\n\n";
+
+    for(int square = 0; square < 64; square++){
+        //init rookmagic numbers
+        //printf(" 0x%llxULL\n", find_magic_number(square, bishop_relevant_bits[square], bishop));
+        bishop_magic_numbers[square] = find_magic_number(square, bishop_relevant_bits[square], bishop);
+    }
+}
+
+/*********************************\ 
+ 
+        init all
+
+ ==================================
+ \*********************************/
+
+ //init all variables
+
+ void init_all(){
+
     init_leapers_attacks();
 
-    print_bitboard((U64)get_random_U32number());
-    print_bitboard((U64)(get_random_U32number() & 0xFFFF)); //slice upper (from MS1b side) 16 bits
-    print_bitboard(get_random_U64number());
-    print_bitboard(generate_magic_number());
+    //init magic nubmers
+    //init_magic_numbers();
+
+ }
+
+
+/*********************************\ 
+ 
+        Main Driver
+
+ ==================================
+ \*********************************/ 
+
+int main(){
+    //init all
+    init_all();
 
     return 0;
 
